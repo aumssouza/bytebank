0c34f1cf2875f27fc9f7b5e07459e62962a9c340
tatzooism <tatzooism@zoho.com>
2021-05-04 19:57:06 -0300

    Changing to object orientation

--- src/main/kotlin/Bank.kt
+++ src/main/kotlin/Bank.kt
@@ -1,13 +1,28 @@
-import java.util.Scanner
+//class for accounts
+class Account(){
+    var titular:String=""
+    var numero:Int=0
+    var saldo:Double=0.0
+    var credito:Double=0.0
+}
 fun main(){
+    //starting account
+        val acesso = Account()
+        print("Input starting money: ")
+        acesso.saldo= readLine().toString().toDouble()
+        print("Input the account owner's name: ")
+        acesso.titular= readLine().toString()
+        print("Input the account number: ")
+        acesso.numero= Integer.valueOf(readLine().toString())
+
     //setting variables for run
         //numeric ones
         var operation=0 //default value to not run any operation
-        print("Input starting money: ")
-        var saldo=readLine().toString().toDouble()
+
         var operationCost:Double= 0.0
         //Text
         val welcome= "• Welcome to ByteBank •"
+        val welcome2= "• Hello ${acesso.titular} •"
         val operationCheck= "• What do you want to do? •"
         val exit= "• Have a good day! •"
         val currentSaldo="You currently have R$ "
@@ -15,15 +30,12 @@ fun main(){
 
     //run
     var run:Int= 0 //used to keep the bank loop running
-    print("Use bank as: ")
-    val user= readLine()
-
     println("") //used for spacing
     //bank welcomes log in
         println(welcome)
-        println("You're logged in as $user")
-        println(currentSaldo + saldo)
-        if(saldo<0) println("-= You're in debt! =-")
+        println("You're logged in as ${acesso.titular}")
+        println(currentSaldo + acesso.saldo)
+        if(acesso.saldo<0) println("-= You're in debt! =-")
         println("====")
     //bank operations after logging in
         while(run==0){
@@ -33,33 +45,33 @@ fun main(){
                             print("Input the value you're going to deposit: ")
                             operationCost=readLine().toString().toDouble()
                             println(operationCost)
-                            saldo+=operationCost
+                            acesso.saldo+=operationCost
                             println("-= Deposit successful! =-")
-                            println(currentSaldo +saldo)
-                            if(saldo<0) println("-= You're in debt! =-")
+                            println(currentSaldo + acesso.saldo)
+                            if(acesso.saldo<0) println("-= You're in debt! =-")
                             operation = 0
                         }
                         operation==2->{
                             println("=====")
                             print("Choose the value that you're going to Withdraw: ")
                             operationCost=readLine().toString().toDouble()
-                            if(saldo>=operationCost){
-                                saldo-=operationCost
+                            if(acesso.saldo>=operationCost){
+                                acesso.saldo-=operationCost
                                 println("-= Withdrawal successful! =-")
-                                println(currentSaldo + saldo)
+                                println(currentSaldo + acesso.saldo)
                             }else{
                                 println("You don't have enough money in your account")
                                 print("Still want to withdraw? Y/N - ")
                                 confirmChoice= readLine().toString()
                                 if(confirmChoice == "Y"){
-                                    saldo-=operationCost
+                                    acesso.saldo-=operationCost
                                     println("-= Withdrawal successful! =-")
-                                    println(currentSaldo + saldo)
+                                    println(currentSaldo + acesso.saldo)
                                     println("-= You're in debt! =-")
                                 } else if(confirmChoice=="N"){
                                     println("-= Withdrawal canceled! =-")
-                                    println(currentSaldo + saldo)
-                                    if(saldo<0)println("-= You're in debt! =-")
+                                    println(currentSaldo + acesso.saldo)
+                                    if(acesso.saldo<0)println("-= You're in debt! =-")
                                 }
                             }
                             operation=0
