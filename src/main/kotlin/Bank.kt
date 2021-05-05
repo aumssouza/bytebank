df6602ea217668ed2c1b45ec1c6477c84905f008
tatzooism <tatzooism@zoho.com>
2021-05-04 21:38:43 -0300

    Created operations class to host all account operations and an easier way to do Yes or No checks

--- src/main/kotlin/Bank.kt
+++ src/main/kotlin/Bank.kt
@@ -5,8 +5,39 @@ class Account(){
     var saldo:Double=0.0
     var credito:Double=0.0
 }
+class Operations(){
+    var value:Double=0.0
+    fun deposit(account:Account){
+        value= readLine().toString().toDouble()
+        account.saldo+=value
+    }
+    fun withdraw(account: Account){
+        value= readLine().toString().toDouble()
+        if(account.saldo>=value){
+            account.saldo-=value
+            println("-= Withdrawal Successful! =-")
+        }else{
+            println("You're currently in debt! \n Are you sure you want to withdraw? (Y/N)")
+            if(operationCheck()=="Y"){
+                account.saldo-=value
+                println("-= Withdrawal Successful! =-")
+            }else{
+                println("-= Withdrawal Canceled! =-")
+            }
+        }
+    }
+    fun exit():Int{
+        println("Are you sure you want to exit?(Y/N)")
+        return if(operationCheck()=="Y"){
+            1
+        } else{
+            0
+        }
+    }
+}
 fun main(){
     //starting account
+        val operations = Operations()
         val acesso = Account()
         print("Input starting money: ")
         acesso.saldo= readLine().toString().toDouble()
@@ -18,16 +49,12 @@ fun main(){
     //setting variables for run
         //numeric ones
         var operation=0 //default value to not run any operation
-
-        var operationCost:Double= 0.0
         //Text
         val welcome= "• Welcome to ByteBank •"
         val welcome2= "• Hello ${acesso.titular} •"
         val operationCheck= "• What do you want to do? •"
         val exit= "• Have a good day! •"
         val currentSaldo="You currently have R$ "
-        var confirmChoice:String = "aaa"
-
     //run
     var run:Int= 0 //used to keep the bank loop running
     println("") //used for spacing
@@ -42,51 +69,23 @@ fun main(){
                 when {
                         operation==1->{
                             println("=====")
-                            print("Input the value you're going to deposit: ")
-                            operationCost=readLine().toString().toDouble()
-                            println(operationCost)
-                            acesso.saldo+=operationCost
-                            println("-= Deposit successful! =-")
+                            print("Choose the value that you're going to deposit: R$")
+                            operations.deposit(acesso)
                             println(currentSaldo + acesso.saldo)
                             if(acesso.saldo<0) println("-= You're in debt! =-")
                             operation = 0
                         }
                         operation==2->{
                             println("=====")
-                            print("Choose the value that you're going to Withdraw: ")
-                            operationCost=readLine().toString().toDouble()
-                            if(acesso.saldo>=operationCost){
-                                acesso.saldo-=operationCost
-                                println("-= Withdrawal successful! =-")
-                                println(currentSaldo + acesso.saldo)
-                            }else{
-                                println("You don't have enough money in your account")
-                                print("Still want to withdraw? Y/N - ")
-                                confirmChoice= readLine().toString()
-                                if(confirmChoice == "Y"){
-                                    acesso.saldo-=operationCost
-                                    println("-= Withdrawal successful! =-")
-                                    println(currentSaldo + acesso.saldo)
-                                    println("-= You're in debt! =-")
-                                } else if(confirmChoice=="N"){
-                                    println("-= Withdrawal canceled! =-")
-                                    println(currentSaldo + acesso.saldo)
-                                    if(acesso.saldo<0)println("-= You're in debt! =-")
-                                }
-                            }
+                            print("Choose the value that you're going to Withdraw: R$")
+                            operations.withdraw(acesso)
+                            println(currentSaldo + acesso.saldo)
+                            if(acesso.saldo<0) println("-= You're in debt! =-")
                             operation=0
                         }
                         operation==3->{
                             println("=====")
-                            println("Are you sure you want to exit? Y/N")
-                            confirmChoice= readLine().toString()
-                            if(confirmChoice=="Y") {
-                                println(exit)
-                                run = 1
-                            }else{
-                                println("=====")
-                                operation=0
-                            }
+                            operation=operations.exit()
                         }
                         else ->{
                             println(operationCheck)
@@ -100,4 +99,9 @@ fun main(){
                         }
                 }
         }
+        println(exit)
+}
+//Function to save lines when doing Yes/No checks
+fun operationCheck():String{
+    return readLine().toString()
 }
